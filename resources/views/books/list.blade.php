f6392602bae35fd53ab4485d4fed978216923da8
cuonggio003 <nguyenquoccuongvntn2003@gmail.com>
2021-12-09 09:55:11 +0700

    user

--- resources/views/books/list.blade.php
+++ resources/views/books/list.blade.php
@@ -1,35 +1,25 @@
-@extends('layouts.app')
+@extends('master')
 @section('title', 'Danh sách ')
 @section('content')
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
+    <div class="pcoded-content">
 
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Danh sách</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">
-                                <a class="btn btn-success" href="{{ route('books.create') }}">Thêm mới</a>
-                            </h3>
-                        </div>
-                        <!-- /.card-header -->
+        <section class="content">
+            <div class="main-container">
+                <div class="xs-pd-20-10 pd-ltr-20">
+                    <div class="card mt-2">
+                        <h5 class="card-header">Danh sách sách
+                            <a class="btn btn-outline-warning" href="{{ route('books.create') }}">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z">
+                                    </path>
+                                    <path fill-rule="evenodd"
+                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z">
+                                    </path>
+                                </svg>
+                            </a>
+                        </h5>
                         <div class="card-body">
                             <table id="example1" class="table table-bordered table-striped">
                                 <thead>
@@ -39,7 +29,7 @@
                                         <th>Mô tả</th>
                                         <th>Ảnh</th>
                                         <th>Trạng thái</th>
-                                        <th>Giá</th>
+                                        <th>Giá VND</th>
                                         <th>Thể loại</th>
                                         <th colspan="2">Hoạt động</th>
 
@@ -53,8 +43,8 @@
                                             <td>{{ $book->desc }}</td>
                                             <td>
                                                 @if ($book->image)
-                                                    <img src="{{ asset('storage/' . $book->image) }}" alt="{{ asset('storage/' . $book->image) }}"
-                                                        style="width: 150px">
+                                                    <img src="{{ asset('storage/' . $book->image) }}"
+                                                        alt="{{ asset('storage/' . $book->image) }}" style="width: 150px">
                                                 @else
                                                     {{ 'chưa có ảnh ' }}
                                                 @endif
@@ -69,11 +59,28 @@
                                             </td>
 
                                             <td>
-                                                <a href="{{ route('books.edit', ['id' => $book->id]) }}"
-                                                    class="btn btn-primary"><i class="fas fa-edit"></i></a>
-                                                <a onclick="return confirm('bạn có muốn xóa {{ $book->name }} không?')"
-                                                    href="{{ route('books.delete', ['id' => $book->id]) }}"
-                                                    class="btn btn-danger"><i class="fas fa-trash"></i></a>
+                                                <a class="btn btn-outline-primary"
+                                                    href="{{ route('books.edit', ['id' => $book->id]) }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z">
+                                                        </path>
+                                                    </svg>
+                                                </a>
+                                               
+                                                <a class="btn btn-outline-danger" onclick="return confirm('bạn có muốn xóa {{ $book->name }} không?')"
+                                                    href="{{ route('books.delete', ['id' => $book->id]) }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z">
+                                                        </path>
+                                                        <path fill-rule="evenodd"
+                                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z">
+                                                        </path>
+                                                    </svg>
+                                                </a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -87,8 +94,8 @@
                 <!-- /.col -->
             </div>
             <!-- /.row -->
-        </div>
-        <!-- /.container-fluid -->
+    </div>
+    <!-- /.container-fluid -->
     </section>
-    <!-- /.content -->
+    </div>
 @endsection
